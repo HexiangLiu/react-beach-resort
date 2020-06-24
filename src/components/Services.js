61507776f0ea94e6fff047163e1631d1e9092291
Henry Liu <hl3184@columbia.edu>
2020-06-23 23:46:32 -0400

    Services Part Completed

---
+++ src/components/Services.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
+
+import './Services.scss';
+
+export default function Services() {
+  const services = [
+    {
+      icon: <FaCocktail />,
+      title: 'Free Cocktail',
+      info:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, illo!',
+    },
+    {
+      icon: <FaHiking />,
+      title: 'Endless Hiking',
+      info:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, illo!',
+    },
+    {
+      icon: <FaShuttleVan />,
+      title: 'Free shuttle',
+      info:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, illo!',
+    },
+    {
+      icon: <FaBeer />,
+      title: 'Strongest Beer',
+      info:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, illo!',
+    },
+  ];
+
+  return (
+    <div className="services">
+      <div className="title">
+        <h2 className="heading--2">services</h2>
+      </div>
+
+      <div className="services__detail">
+        {services.map((service, index) => {
+          return (
+            <article className="service" key={index}>
+              <span className="service__icon">{service.icon}</span>
+              <h3 className="service__title">{service.title}</h3>
+              <p className="service__info">{service.info}</p>
+            </article>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,22 @@ import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Banner from '../components/Banner';
+import Services from '../components/Services';
 
 export default function Home() {
   return (
-    <Header>
-      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-        <Link to="/rooms" className="btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </Header>
+    <>
+      <Header>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Header>
+      <Services />
+    </>
   );
 }
